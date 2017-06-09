882b0127299749afc49471366f28694a77ad35b2
dkniazevych <dkniazevych@hiqinvest.nl>
2017-06-09 09:45:59 +0200

    project build

---
+++ lib/sax.d.ts
@@ -0,0 +1,2 @@
+import { Handler } from 'htmlparser2';
+export declare function parseXML(content: string, handler: Handler): void;
--- src/sax.ts
+++ src/sax.ts
@@ -1,6 +1,6 @@
 import {Parser, Handler} from 'htmlparser2';
 
-export function parseXML (content: string, handler: Handler) {
+export function parseXML (content: string, handler: Handler): void {
     const parser: Parser = new Parser(handler, {
         recognizeSelfClosing: true,
         lowerCaseTags: false,
