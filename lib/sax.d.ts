ad62297f278e44ed3d61be391fee0ea041980562
dkniazevych <dkniazevych@hiqinvest.nl>
2017-08-28 12:50:43 +0200

    add options

--- lib/sax.d.ts
+++ lib/sax.d.ts
@@ -1,2 +1,2 @@
-import { Handler } from 'htmlparser2';
-export declare function parseXML(content: string, handler: Handler): void;
+import { Handler, Options } from 'htmlparser2';
+export declare function parseXML(content: string, handler: Handler, options?: Options): void;
--- src/sax.ts
+++ src/sax.ts
@@ -1,11 +1,11 @@
-import {Parser, Handler} from 'htmlparser2';
+import {Parser, Handler, Options} from 'htmlparser2';
 
-export function parseXML (content: string, handler: Handler): void {
-    const parser: Parser = new Parser(handler, {
+export function parseXML (content: string, handler: Handler, options?: Options): void {
+    const parser: Parser = new Parser(handler, Object.assign({
         recognizeSelfClosing: true,
         lowerCaseTags: false,
         lowerCaseAttributeNames: false
-    });
+    }, options));
 
     parser.write(content);
     parser.end();
